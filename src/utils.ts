95528ceb6a2545144fc5be38fe8bada06f652636
Byeong Gyu Choi <bisschoi9541@gmail.com>
2018-03-14 13:28:05 +0900

    Implemented retrieve actions

--- src/utils.ts
+++ src/utils.ts
@@ -2,9 +2,9 @@ import { APIGatewayEvent } from 'aws-lambda';
 import { DynamoDB } from 'aws-sdk';
 
 import * as uuid from 'uuid/v1';
-import { LambdaHttpResponse, UserInfo } from './index';
+import { InsertionParameter, LambdaHttpResponse, ScanParameter, UserInfo } from './index';
 
-export function generateUserParams(body: string): DynamoDB.DocumentClient.PutItemInput {
+export function generateUserParams(body: string): InsertionParameter {
   const newUser: UserInfo = JSON.parse(body);
   newUser.uuid = uuid();
 
@@ -12,6 +12,23 @@ export function generateUserParams(body: string): DynamoDB.DocumentClient.PutIte
     TableName: 'sample-userdb',
     Item: newUser,
   };
+}
+
+export function generateScanParams(body?: string | null, query = false): ScanParameter {
+  const parameter: ScanParameter = {
+    TableName: 'sample-userdb',
+  };
+
+  if (query) {
+    parameter.FilterExpression = '#id = :username and #pw = :password';
+    parameter.ExpressionAttributeNames = {
+      '#id': 'username',
+      '#pw': 'password',
+    };
+    parameter.ExpressionAttributeValues = JSON.parse(body);
+  }
+
+  return parameter;
 }
 
 export function generateResponse(status: number, body: object): LambdaHttpResponse {
